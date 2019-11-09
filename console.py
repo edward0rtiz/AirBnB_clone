01d43556f0e7f7a78197a336f4466ea7cd644b68
edward0rtiz <931@holbertonschool.com>
2019-11-09 11:47:52 -0500

    #8 [fix] update console.py __classes attribute

--- console.py
+++ console.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
 import cmd
+
+from models import storage
+from models.base_model import BaseModel
+from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+
 """Console to
 manage
 hbnb data
@@ -8,6 +18,18 @@ hbnb data
 
 class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
+    __classes = {
+        'BaseModel',
+        'User',
+        'Amenity',
+        'City',
+        'Place',
+        'Review',
+        'State'
+    }
+
+    def emptyline(self):
+        pass
 
     def do_quit(self, line):
         """Exits the shell"""
